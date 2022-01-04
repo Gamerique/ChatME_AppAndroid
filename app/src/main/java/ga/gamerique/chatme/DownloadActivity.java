cc6da7f6ec9e773feadf6ac877a3097ccb69e420
Rishi <rishiraj11a@gmail.com>
2022-01-04 17:24:22 +0530

    Designing Login and Signup Page

--- app/src/main/java/ga/gamerique/chatme/DownloadActivity.java
+++ app/src/main/java/ga/gamerique/chatme/DownloadActivity.java
@@ -2,13 +2,39 @@ package ga.gamerique.chatme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DownloadActivity extends AppCompatActivity {
+
+    Button btnSignInPage, btnSignUpPage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
+
+        btnSignInPage= findViewById(R.id.btnSignInPage);
+        btnSignUpPage= findViewById(R.id.btnSignUpPage);
+
+
+        btnSignInPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DownloadActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignUpPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DownloadActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/ga/gamerique/chatme/SplashActivity.java
+++ app/src/main/java/ga/gamerique/chatme/SplashActivity.java
@@ -2,6 +2,7 @@ package ga.gamerique.chatme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,5 +11,25 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        Thread thread = new Thread(){
+
+            public void run(){
+                try {
+                    sleep(2000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(SplashActivity.this, DownloadActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        };thread.start();
+
     }
 }
